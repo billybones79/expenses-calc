328f6548f28154b10987a05ebffa376cf285cb33
Billybones79 <alexandredt79@gmail.com>
2016-01-13 22:21:55 -0500

    clearer ben du stock de wsgi

--- wsgi.py
+++ wsgi.py
@@ -38,31 +38,9 @@ if __name__ == '__main__':
   app_name = application.app.config['APP_NAME']
   host_name = application.app.config['HOST_NAME']
 
-  fwtype="wsgiref"
-  for fw in ("gevent", "cherrypy", "flask"):
-    try:
-      imp.find_module(fw)
-      fwtype = fw
-    except ImportError:
-      pass
+  print('Starting WSGIServer type %s on %s:%d ... ' % ("flask", ip, port))
 
-  print('Starting WSGIServer type %s on %s:%d ... ' % (fwtype, ip, port))
-  if fwtype == "gevent":
-    from gevent.pywsgi import WSGIServer
-    WSGIServer((ip, port), application.app).serve_forever()
-
-  elif fwtype == "cherrypy":
-    from cherrypy import wsgiserver
-    server = wsgiserver.CherryPyWSGIServer(
-      (ip, port), application.app, server_name=host_name)
-    server.start()
-
-  elif fwtype == "flask":
-    from flask import Flask
-    server = Flask(__name__)
-    server.wsgi_app = application.app
-    server.run(host=ip, port=port)
-
-  else:
-    from wsgiref.simple_server import make_server
-    make_server(ip, port, application.app).serve_forever()
+  from flask import Flask
+  server = Flask(__name__)
+  server.wsgi_app = application.app
+  server.run(host=ip, port=port)
