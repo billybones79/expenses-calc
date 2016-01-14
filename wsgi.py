04ff4c62ac51a944c2cb79980a00a23346065cab
Billybones79 <alexandredt79@gmail.com>
2016-01-13 22:29:05 -0500

    wsgi plus pertinent un peu

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,8 @@
 import imp
 import os
 import sys
+import logging, sys
+logging.basicConfig(stream=sys.stderr)
 
 try:
   virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
@@ -26,7 +28,8 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-
+def application(environ, start_response)
+    from flaskapp import app as application
 
 #
 #  main():
