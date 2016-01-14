f11dd2f0adf0b2be0989a4e0df6c54d6619981d7
Billybones79 <alexandredt79@gmail.com>
2016-01-13 22:40:28 -0500

    wsgi plus pertinent un peu

--- wsgi.py
+++ wsgi.py
@@ -28,8 +28,8 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-def application(environ, start_response):
-    from expenses import app as application
+
+from expenses import app as application
 
 #
 #  main():
